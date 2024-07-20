b2f3eeebc639117dbc736a31c3b51f7bceb65c4d
Xolisa Nunge <xolinunge0@gmail.com>
2024-07-20 16:06:14 +0200

    Created pages and routes

--- src/App.js
+++ src/App.js
@@ -1,9 +1,28 @@
+import { BrowserRouter as Router , Routes , Route } from "react-router-dom";
+import Home from "./pages/Home";
+import Offers from "./pages/Offers";
+import Profile from "./pages/Profile";
+import Favorites from "./pages/Favorites";
+import SignIn from "./pages/SignIn";
+import SignUp from "./pages/SignUp";
+import ForgotPassword from "./pages/ForgotPassword";
+
 
 function App() {
   return (
     <div className="App">
       <>
-      <h1>Hello World</h1>
+        <Router>
+          <Routes>
+              <Route path="/" element={<Home/>}></Route>
+              <Route path="/profile" element={<Profile/>}></Route>
+              <Route path="/offers" element={<Offers/>}></Route>
+              <Route path="/favorites" element={<Favorites/>}></Route>
+              <Route path="/sign-in" element={<SignIn/>}></Route>
+              <Route path="/sign-up" element={<SignUp/>}></Route>
+              <Route path="/forgot-password" element={<ForgotPassword/>}></Route>
+          </Routes>
+        </Router>
       </>
       
     </div>
